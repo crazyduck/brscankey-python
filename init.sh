da234b91005d65ac01229f0246197ed06e8670d3
Maxi <maxi@krause.re>
2024-06-02 15:18:55 +0200

    entrypoint start scand

--- init.sh
+++ init.sh
@@ -3,6 +3,9 @@
 /usr/bin/scanimage -L
 
 # keep container for debug purposes awake
-while true; do
-    sleep 1
-done
+# while true; do
+#     sleep 1
+# done
+echo "Starting brscand"
+
+brscand -A 192.168.10.25 192.168.10.20
