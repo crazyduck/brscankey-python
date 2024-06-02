d2dbfb6fdde9b1e3061e6a179c60a108b4aa1de9
Maxi <maxi@krause.re>
2024-06-02 15:33:40 +0200

    enable debug statement in init.sh

--- init.sh
+++ init.sh
@@ -3,9 +3,9 @@
 /usr/bin/scanimage -L
 
 # keep container for debug purposes awake
-# while true; do
-#     sleep 1
-# done
-echo "Starting brscand"
+while true; do
+    sleep 1
+done
+# echo "Starting brscand"
 
-brscand -A 192.168.10.25 192.168.10.20
+# brscand -A 192.168.10.25 192.168.10.20
